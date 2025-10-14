023f9ba69de0f80c09daa7bd5dbf1f998c240d1b
Paul Clark <paul.clark@edu.ece.fr>
2025-10-14 11:34:57 +0200

    Delete rag-qwen-faiss/src/embeddings.py

--- rag-qwen-faiss/src/embeddings.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-def load_embedder():
-    print("Chargement du modèle Qwen3-Embedding...")
-    return SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")
-
-def embed_texts(texts, model):
-    print(f"Encodage de {len(texts)} textes...")
-    return model.encode(texts, convert_to_numpy=True)
